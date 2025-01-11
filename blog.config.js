382e90dee55c7b9d35a8773adc0d040f45af496d
StarryFeatheys <162771613+StarryFeatheys@users.noreply.github.com>
2025-01-11 18:29:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -336,21 +336,35 @@ const BLOG = {
       url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
       cover:
         'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    }
+    },
+     {
+      name: '叹云兮',
+      artist: 'Falcom Sound Team jdk',
+      url: 'http://m10.music.126.net/20250111184438/53064571fae54be054fe785cf3017c15/ymusic/d9eb/6b12/7720/7892aec1dd9ab8b822e2d9a2087bec64.mp3?vuutv=o6xJg/nxKRQrxwfp9RXt262Mqbk3VcqzP9E09LqV8eBQPvUasUXiuKCwwpPQ3szpJh4qWZn9kTRHC/8pylX5985ByMvCABb+7Bg77LAfcix/kZgdG4iCawpkxz9u7boiW1JtwK0VHocG3ohrSB2inrcsHA9DHCas/9A8UQ9od0/cPkHdeWiX22aj2U3PVujqo8rXov/ujsDXNxyUyuLHX35zjLnWpxyLxcMCmy8zs2xdr8Mwlx9cXkFGHowY9s9cHqkkg8r+bCaOOZKyPEhXnO0Da2Lez4DxYjuFpZ7MpUPVHg6ooVniKxGVD0uyungEJqXgpfP1TDlr+N37OrsnsgibqBw91jEWC+oSA87WtOxPABSQxgUFwsVJvfld4KD8',
+      cover:
+        'https://p3.music.126.net/80ycl8oJz6UBRsGl7MNRUg==/109951170318437851.jpg?param=300y300',
+    },
+     {
+      name: '遇见(Live)',
+      artist: 'Falcom Sound Team jdk',
+      url: 'http://m701.music.126.net/20250111184738/aafec6e2d6900cdb0dc0ddabaa865d17/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/14096548668/3c57/5806/d5bc/85cef6be8791125e367b438266e2a855.mp3?vuutv=c9lHG9qCc0OUPpfCN/j5iF6fFUcjyK+A8ZzWNstQzuodmhLduNqCYoBv7Fm0DTpWpuCqtbRNVXSdRYkEdUqmoArD4GkbzMJtN+Q2opF62w7CmahIfftI4rG2QVV0vjw6655Go7gjEdvns8DxL1kd7XBkEYEUEBfiDFuZJkMOugtv84O56Yl2VjBqNwyeuNt4adNHQSv69RCRtzqDMsIY6whDQA00/iLdyH6tY3JUKEg3Ch98sguVAQNBaFVycAsfUAnb1xJ9B1rp41AClEIIjTjT3lApya/hq8dtjpTh53jZZkO+LxvXF/hZze+dTMySZV0odHrUK0gG7Mdr5xMCRC2edbLu2EqiwzmNLFAxNonWiIFoXEpeMEg2+IHjqhuL',
+      cover:
+        'https://p3.music.126.net/sF67FVEvm-TpP_-TA9I5vA==/3298534890576644.jpg?param=300y300'
+    },
      {
       name: '叹云兮',
       artist: 'Falcom Sound Team jdk',
       url: 'http://m10.music.126.net/20250111184438/53064571fae54be054fe785cf3017c15/ymusic/d9eb/6b12/7720/7892aec1dd9ab8b822e2d9a2087bec64.mp3?vuutv=o6xJg/nxKRQrxwfp9RXt262Mqbk3VcqzP9E09LqV8eBQPvUasUXiuKCwwpPQ3szpJh4qWZn9kTRHC/8pylX5985ByMvCABb+7Bg77LAfcix/kZgdG4iCawpkxz9u7boiW1JtwK0VHocG3ohrSB2inrcsHA9DHCas/9A8UQ9od0/cPkHdeWiX22aj2U3PVujqo8rXov/ujsDXNxyUyuLHX35zjLnWpxyLxcMCmy8zs2xdr8Mwlx9cXkFGHowY9s9cHqkkg8r+bCaOOZKyPEhXnO0Da2Lez4DxYjuFpZ7MpUPVHg6ooVniKxGVD0uyungEJqXgpfP1TDlr+N37OrsnsgibqBw91jEWC+oSA87WtOxPABSQxgUFwsVJvfld4KD8',
       cover:
         'https://p3.music.126.net/80ycl8oJz6UBRsGl7MNRUg==/109951170318437851.jpg?param=300y300'
-    }
+    },
      {
       name: '云还在唱歌',
       artist: 'Falcom Sound Team jdk',
       url: 'http://m701.music.126.net/20250111184028/667056fd8bce892a24b5bfc8bbe3ddaf/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/14047864335/fb6e/6d5c/eba0/2e6299de11158c892a6183bb149d2d67.mp3?vuutv=s4Odg7TpMjOBGw3ti/+wWL06vnHJPQsPDOa5HwHUDL+rmlm3Ihi0xMg8yxYrVYb2YjcInLgEnxHUsia81E0jB/KQ7FUw1ZMHXRbDLfbB+VGYPfbre3cProhWUCof9/8DUrKsO4VtXZ3tqB3/tqPtKrOlE5RfA0nv7W8RGVX6Rc+dwat8H+Sm+cYfGSn5j85mI+h/1rnEAxV47ZnXBBpx+Q2ryfA79nxT8ZTF7l2p/TjBc73+EzX+t+SnBIbqn/xoMyJeGszmAM47S7otQR43Rp0I05DfD3bBFXL6orATeBDVIreqWCEFVDpqH0wcVXRYt0jB/NRxKtBHpm0jyFHZGh8/BW8k8G1sKpl5YSSHfDRgBeCxrrzgcb+LHfqYrNEE',
       cover:
         'https://p3.music.126.net/CtZG5qxH9LPgGV_PThqSBg==/109951167322582296.jpg?param=300y300'
-    }
+    },
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
